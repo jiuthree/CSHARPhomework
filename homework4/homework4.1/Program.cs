7a25ef04ac5686e254ecf768d8bc5849410e1545
hzh <2549927770@qq.com>
2020-03-09 10:57:22 +0800

    第四次C#作业

---
+++ homework4/homework4.1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4._1
+{   public class Node<T> {
+public Node<T> Next { set; get; }
+public T Data { set; get; }
+public Node(T t) { Next = null;Data = t; }
+
+    }
+
+    public class GenericList<T> {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList() { tail = head = null; }
+        public Node<T>Head{ get => head; }
+       
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null) { head = tail = n; }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action) {
+            Node<T> temp =head;
+            if (temp == null) return;
+            action(temp.Data);
+            temp = temp.Next;
+            while (temp!=tail) {
+                action(temp.Data);
+                temp = temp.Next;
+
+
+            }
+            action(temp.Data);
+            temp = temp.Next;
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        { Action<int> action = (m => Console.WriteLine(m));
+            GenericList<int> list = new GenericList<int>();
+            int sum=0;int max=0;int min=0;
+            for (int x = 0; x < 10; x++)
+            {
+                list.Add(x);
+            }        
+            list.ForEach(action);
+            list.ForEach(m=>sum += m);
+            list.ForEach(m =>max=( max >m) ? max : m);
+            list.ForEach(m => min = (min < m) ? min : m);
+            Console.WriteLine(sum);
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+        }
+    }
+}
---
+++ homework4/homework4.2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4._2
+{
+    public delegate void ClockHander(object sender, Clock args);
+    public class Clock {public int Time { set; get; }
+
+    }
+    public class Handevent
+    {
+        public event ClockHander Tick;
+        public event ClockHander Alarm;
+        public void Clocks(int t)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+
+
+                Clock args = new Clock() { Time = t };
+                Tick(this, args);
+                Console.WriteLine($"时间是{t}秒");
+            if (t % 5 == 0) { Alarm(this, args); };
+                t++;
+           
+            }
+
+        }
+    }
+        public class Control
+        {
+            public Handevent hand1 = new Handevent();
+            public Control()
+            {
+                hand1.Tick += new ClockHander(Tick1);
+                hand1.Alarm += new ClockHander(Alarm1);
+                void Alarm1(object sender, Clock args)
+            { Console.WriteLine("Alarm"); }
+                void Tick1(object sender, Clock args)
+                {
+
+
+                    System.Threading.Thread.Sleep(1000);
+                    Console.WriteLine("Tick发生");
+
+
+                }
+
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Control con = new Control();
+                con.hand1.Clocks(1);
+
+            }
+        }
+    }        
