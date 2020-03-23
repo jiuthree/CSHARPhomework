7a0caf2b4d8ee3f66695a6cbe37fb18051d4eb4d
hzh <2549927770@qq.com>
2020-03-23 10:47:40 +0800

    第六次C#作业

---
+++ homework6/OrderServiceTests.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using homework5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace homework5.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        
+      public  OrderService service = new OrderService();
+      public  Order order1 = new Order();
+      public  Order order2 = new Order();
+        [TestInitialize]
+
+        public  void Intialize()
+        {
+            
+
+            order1.OrderID = 1 + 100;
+            order1.Customername = "客户" + 1;
+            for (int j = 0; j < 3; j++)
+            {
+                OrderItem item = new OrderItem();
+                item.Amount = 3;
+                item.Price = 9;
+                item.Itemname = "商品" + j;
+                item.ItemID = 100 + j;
+                order1.AddOrderItem(item);
+
+            }
+
+            order1.CountTotal();
+            service.Add(order1);
+
+
+
+
+            order2.OrderID = 1 + 100;
+            order2.Customername = "客户" + 2;
+            for (int j = 0; j < 3; j++)
+            {
+                OrderItem item = new OrderItem();
+                item.Amount = 3;
+                item.Price = 9;
+                item.Itemname = "商品" + j;
+                item.ItemID = 100 + j;
+                order2.AddOrderItem(item);
+
+            }
+
+            order2.CountTotal();
+
+
+        }
+       
+        [TestMethod()]
+        public void ExportTest()
+        {
+            service.Export();
+            FileStream fs = new FileStream(@"D:/s.xml", FileMode.Open);
+            Assert.IsNotNull(fs);
+        }
+
+        [TestMethod()]
+        public void ImportTest()
+        {
+            service.Import("D:/s.xml");
+            Assert.IsNotNull(service.lists);
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            
+            service.Remove(order1);
+            service.Add(order2);
+            Assert.IsNotNull(service.lists);
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            service.Remove(order1);
+            Assert.IsNotNull(service.lists);
+        }
+
+        [TestMethod()]
+        public void ShowOrderListsTest()
+        {
+          
+            Assert.IsTrue(service.ShowOrderLists());
+        }
+
+        [TestMethod()]
+        public void SortTest()
+        {
+            Assert.IsTrue(service.Sort());
+        }
+
+        [TestMethod()]
+        public void QuerybyorderIDTest()
+        {
+            Assert.IsTrue(service.QuerybyorderID(0,200));
+        }
+
+        [TestMethod()]
+        public void QuerybyCustomernameTest()
+        {
+            Assert.IsTrue(service.QuerybyCustomername("客户1"));
+        }
+
+        [TestMethod()]
+        public void ModifyOrderTest()
+        {
+      
+            Assert.IsTrue(service.ModifyOrder(101,order2));
+        }
+    }
+}
---
+++ homework6/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            OrderService service = new OrderService();
+            for (int i = 0; i < 3; i++) {
+                Order order=new Order();
+                order.OrderID = i+100;
+                order.Customername = "客户" + i;
+                for (int j = 0; j < 3; j++) {
+                    OrderItem item = new OrderItem();
+                    item.Amount = random.Next(9) + 1;
+                    item.Price = random.Next(100) + 9;
+                    item.Itemname = "商品" + j;
+                    item.ItemID = 100 + j;
+                    order.AddOrderItem(item);
+
+                }
+
+                order.CountTotal();   
+                service.Add(order);
+
+            }
+            service.ShowOrderLists();
+            service.Export();
+            service.Import("D:/s.xml");
+        }
+    }
+    [Serializable]
+   public class Order {
+        public int OrderID { set; get; }
+        public string Customername { set; get; }
+        
+        public List<OrderItem> orderlist= new List<OrderItem> { };
+        public void AddOrderItem(OrderItem oi) { orderlist.Add(oi);   }
+        public void RemoveOrderItem(OrderItem oi) { orderlist.Remove(oi); }
+        public void RemoveAll() { orderlist.Clear(); }
+        public override string ToString()
+        {
+            return "订单号： " + OrderID + "客户名： " + Customername + "总价： " + Total;
+        }
+        public override bool Equals(object obj)
+        {
+            var order = obj as Order;
+            return order != null &&
+                   OrderID == order.OrderID &&
+                   Customername == order.Customername;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1903320604;
+            hashCode = hashCode * -1521134295 + OrderID.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Customername);
+            return hashCode;
+        }
+        
+        public int Total { get; set; }
+        public void CountTotal() {
+            foreach (var item in orderlist)
+            {
+                Total += item.Price * item.Amount;
+            }
+        }
+
+    }
+  public class OrderItem {
+        public int ItemID { set; get; }
+        public int Amount { set; get; }
+        public string Itemname { set; get; }
+        public int Price { set; get; }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as OrderItem;
+            return item != null &&
+                   ItemID == item.ItemID &&
+                   Amount == item.Amount &&
+                   Itemname == item.Itemname &&
+                   Price == item.Price;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 2036580019;
+            hashCode = hashCode * -1521134295 + ItemID.GetHashCode();
+            hashCode = hashCode * -1521134295 + Amount.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Itemname);
+            hashCode = hashCode * -1521134295 + Price.GetHashCode();
+            return hashCode;
+        }
+        public override string ToString()
+        {
+            return "商品号：" + ItemID + "商品名：" + Itemname + "单价：" + Price + "数量：" + Amount;
+        }
+    }
+  public  class OrderService {
+        public List<Order> lists=new List<Order> { };
+        public void Export() {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Order>));
+            using (FileStream fs = new FileStream(@"D:/s.xml", FileMode.Create)) {
+                xmlSerializer.Serialize(fs, lists);
+            }
+            Console.WriteLine(File.ReadAllText(@"D:/s.xml"));
+        }
+        public void Import(string path) {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Order>));
+            using (FileStream fs = new FileStream(@path, FileMode.Open)) {
+                List<Order> lists1 = (List<Order>)xmlSerializer.Deserialize(fs);
+                lists = lists1;
+                ShowOrderLists();
+
+            } }
+        public void Add(Order od) { lists.Add(od); }
+        public void Remove(Order od) {
+            od.RemoveAll();
+            lists.Remove(od); }
+        public Boolean ShowOrderLists()
+        {
+            foreach (var order in lists)
+            {
+                Console.WriteLine(order.ToString());
+                foreach (var oi in order.orderlist)
+                {
+                    Console.WriteLine(oi.ToString());
+
+                }
+            }
+            return true;
+        }
+        public Boolean Sort() { lists.Sort((o1, o2) => o1.OrderID - o2.OrderID);return true; }
+        public Boolean QuerybyorderID(int n1,int n2) {
+            var query =from order in lists
+                       where order.OrderID <= n2 && order.OrderID >= n1
+                       orderby order.Total
+                       select order;
+            List<Order> list = query.ToList();
+            foreach (var order in list)
+            {
+                Console.WriteLine(order.ToString());
+                foreach (var oi in order.orderlist)
+                {
+                    Console.WriteLine(oi.ToString());
+
+                }
+            }
+            return true;
+        }
+        public Boolean QuerybyCustomername(string name) {
+            var query = from order in lists
+                        where order.Customername == name
+                        orderby order.Total
+                        select order;
+            List<Order> list = query.ToList();
+            foreach (var order in list)
+            {
+                Console.WriteLine(order.ToString());
+                foreach (var oi in order.orderlist)
+                {
+                    Console.WriteLine(oi.ToString());
+
+                }
+            }
+            return true;
+        }
+        public Boolean ModifyOrder(int orderid,Order od) {
+            for (int i=0;i<lists.Count;i++) {
+                if (lists[i].OrderID == orderid) { lists.Remove(lists[i]);lists.Add(od); }
+            }
+            return true;
+        }
+        
+    }
+   
+}
